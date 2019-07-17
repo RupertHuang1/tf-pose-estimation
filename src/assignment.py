9f6402139e12d078f508d9be3e511570dc770e82
ruipu huang <ruipuhuang1@gmail.com>
2019-07-17 13:18:01 +1000

    1

--- src/assignment.py
+++ src/assignment.py
@@ -106,10 +106,10 @@ if __name__ == '__main__':
 
             # TODO ensure it only does this when someone is hailing a taxi.
             # That is, an arm is above their head.
-            hail_taxi(image)
+            hail_taxi(image)    
 
             # Debugging statement: remove before demonstration.
-        print([(POSE_COCO_BODY_PARTS[k], v.x, v.y) for k,v in human.body_parts.items()])
+            print([(POSE_COCO_BODY_PARTS[k], v.x, v.y) for k,v in human.body_parts.items()])
 
         # drawing lines on an image
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
